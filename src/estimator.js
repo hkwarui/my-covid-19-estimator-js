c6e89ecf15fbd059d55d8692c8a308efb3481d0f
hkwarui <warui.henry@gmail.com>
2020-04-08 14:02:12 +0300

    assessment-challange1-01

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,19 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  data;
+  const impact = () => {
+    const currentlyInfected = reportedCases * 10;
+    const infectionsByRequestedTime = currentlyInfected * 1024;
+  };
+
+  const severeImpact = () => {
+    const currentlyInfected = reportedCases * 50;
+    const infectionsByRequestedTime = currentlyInfected * 1024;
+  };
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
