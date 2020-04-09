8d614a19f514d1f6e0727197856e0528926ce82f
Your Name <you@example.com>
2020-04-09 18:53:30 +0300

    challenge1-11

--- src/estimator.js
+++ src/estimator.js
@@ -1,59 +1,24 @@
 const covid19ImpactEstimator = (data) => {
   const { reportedCases, timeToElapse, periodType } = data;
-<<<<<<< HEAD
   const impact = {};
   const severeImpact = {};
 
+  let days = timeToElapse;
   if (periodType === 'months') {
-    timeToElapse = timeToElapse * 30;
-    return timeToElapse;
-=======
-
-  let factor = 0;
-  if (periodType === 'days') {
-    factor = Math.floor(timeToElapse / 3);
->>>>>>> 1c2f1abdb15ed31de49ffc8632db3a1600da2b79
+    days *= 30;
+    return days;
   }
   if (periodType === 'weeks') {
-    timeToElapse = timeToElapse * 7;
-    return timeToElapse;
-  }
-  if (periodType === 'days') {
-    return timeToElapse;
+    days *= 7;
+    return days;
   }
 
-<<<<<<< HEAD
   const factor = 2 ** Math.trunc(timeToElapse / 3);
 
   impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * factor;
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.currentlyInfected * factor;
-=======
-  const impact = () => {
-    const currentlyInfected = reportedCases * 10;
-    const infectionsByRequestedTime = currentlyInfected * 2 ** factor;
-
-    return {
-      currentlyInfected,
-      infectionsByRequestedTime
-    };
-  };
-
-  const severeImpact = () => {
-    const currentlyInfected = reportedCases * 50;
-    const infectionsByRequestedTime = currentlyInfected * 2 ** factor;
-    const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
-
-    return {
-      currentlyInfected,
-      infectionsByRequestedTime,
-      severeCasesByRequestedTime
-    };
-  };
->>>>>>> 1c2f1abdb15ed31de49ffc8632db3a1600da2b79
-
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * factor;
   return {
     data,
     impact,
