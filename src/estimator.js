877c7a37e8f3a3db89bc3fbd3f64775754c65183
Your Name <you@example.com>
2020-04-09 19:50:57 +0300

    challenge1-14

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,21 @@
 const covid19ImpactEstimator = (data) => {
-  const { reportedCases, timeToElapse, periodType } = data;
+  const { reportedCases, periodType } = data;
   const impact = {};
   const severeImpact = {};
 
-  let days = timeToElapse;
   if (periodType === 'months') {
-    days *= 30;
-    return days;
+    data.timeToElapse *= 30;
+    return data.timeToElapse;
   }
   if (periodType === 'weeks') {
-    days *= 7;
-    return days;
+    data.timeToElapse *= 7;
+    return data.timeToElapse;
+  }
+  if (periodType === 'days') {
+    return data.timeToElapse;
   }
 
-  const factor = 2 ** Math.trunc(days / 3);
+  const factor = 2 ** Math.trunc(data.timeToElapse / 3);
 
   impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * factor;
