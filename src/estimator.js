b49919189823efc789eedeb3be7f24cfdf4219ce
Your Name <you@example.com>
2020-04-10 11:57:07 +0300

    challenge3-08

--- src/estimator.js
+++ src/estimator.js
@@ -22,7 +22,7 @@ const covid19ImpactEstimator = (data) => {
   impact.severeCasesByRequestedTime = (infections * 0.15);
   impact.hospitalBedsByRequestedTime = Math.trunc(beds - impact.severeCasesByRequestedTime);
 
-  impact.casesForICUByRequestedTime = Math.trunc((infections * 0.05);
+  impact.casesForICUByRequestedTime = Math.trunc(infections * 0.05);
   impact.casesForVentilatorsByRequestedTime = Math.trunc(infections * 0.02);
   impact.dollarsInFlight = (infections * population * income * days);
 
@@ -33,7 +33,7 @@ const covid19ImpactEstimator = (data) => {
   const severeCases = severeImpact.severeCasesByRequestedTime;
   severeImpact.hospitalBedsByRequestedTime = Math.trunc(beds - severeCases);
   severeImpact.casesForICUByRequestedTime = Math.trunc(severeInfection * 0.05);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc((severeInfection * 0.02);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeInfection * 0.02);
   severeImpact.dollarsInFlight = (severeInfection * population * income * days);
 
   return {
