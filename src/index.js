7f6d67ee0beb817ddf3400caf512667adaf51590
Your Name <you@example.com>
2020-04-12 10:16:30 +0300

    challenge4-04

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import covid19ImpactEstimator from './estimator';
-import UI from './ui';
+
+import covid19ImpactEstimator from './estimator.js';
+import UI from './ui.js';
 
 const data = {
   region: {
@@ -19,9 +20,7 @@ document.getElementById('goEstimate').addEventListener('submit', (e) => {
   e.preventDefault();
   data.timeToElapse = document.getElementById('data-time-to-elapse').value;
   data.reportedCases = document.getElementById('data-reported-cases').value;
-  data.totalHospitalBeds = document.getElementById(
-    'data-total-hospital-beds'
-  ).value;
+  data.totalHospitalBeds = document.getElementById('data-total-hospital-beds').value;
   data.periodType = document.getElementById('data-period-type').value;
   data.population = document.getElementById('data-population').value;
   estimate.impact = covid19ImpactEstimator(data).impact;
