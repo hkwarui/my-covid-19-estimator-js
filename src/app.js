2a7a3274da50fea9361ca721d00430fda4cc220d
Your Name <you@example.com>
2020-04-13 17:26:44 +0300

    challeng5-03

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
     return res.status(200).json({});
   }
-  next();
+  return next();
 });
 
 app.get('/', (req, res) => {
