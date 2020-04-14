4a5e9bc55a613deeff26a6bb3cf95f87b0668398
Your Name <you@example.com>
2020-04-14 18:23:13 +0300

    callenge5-13

--- src/app.js
+++ src/app.js
@@ -76,7 +76,6 @@ app.get('/api/v1/on-covid-19/logs', (req, res) => {
   const readStream = fs.createReadStream(filename);
   res.header('Content-Type', 'text/plain');
   readStream.on('open', () => readStream.pipe(res));
-res.header('Content-Type', 'text/plain')
   return readStream.on('error', (err) => res.end(err));
 });
 module.exports = app;
